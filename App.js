60d6b4463c25c59ee2339e4f8de2b00948ca41a2
Sameer Ali Butt <108467884+sameerbutt28@users.noreply.github.com>
2023-06-06 05:14:37 +0500

    Add files via upload

---
+++ App.js
@@ -0,0 +1,23 @@
+import './App.css';
+import React, {useState} from "react"
+import Forms from "./forms"
+
+function App() 
+{
+
+ 
+  return (
+    <div >
+      <h1 className='devname' >  React forms by Sameer - the butt</h1>
+      <Forms/>
+</div>
+      
+  )
+}
+
+
+
+export default App;
+
+ 
+            
